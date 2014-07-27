278f0f125be1ca5609876fe0cb30e535a0c338c5
Richard <richard.be.jamin@gmail.com>
2014-07-27 16:14:33 -0500

    looks cooler now

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 	require_once( './index/functions.php' );
 	$projects = new Projects('index');
-	// if( isset($_GET['refresh']) ){
+	if( isset($_GET['refresh']) ){
 		$projects->rewrite_json_file();
-	// }
+	}
 
 ?>
 <!DOCtype html>
@@ -13,28 +13,28 @@
 	<title>Public Html Folder</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" type="text/css" href="<?php echo $projects->css_url;?>/style.css" media="all" />
+	<script src="<?php echo $projects->js_url; ?>/jquery.js"></script>
 	<script src="<?php echo $projects->js_url; ?>/angular.js"></script>
 	<script src="<?php echo $projects->js_url; ?>/app.js"></script>
 </head>
-	<body ng-controller="projects">
+	<body ng-controller="projects" class="fadeInDo wnBig animated">
 		<div class="wrap">
 			<form><input type="text" ng-model="query"></form><br/>
 
 
 			<ul class="project-list">
-				<li class="flipInX animated"><a href="http://localhost:3000">localhost:3000</a></li>
-				<li class="flipInX animated" ng-repeat="project in projects | filter:query">
-					<a class="highlight-{{project.highlight}}" href="{{project.url}}">{{project.name}}</a>
+				<li class="swing animated"><a href="http://localhost:3000">localhost:3000</a></li>
+				<li class="swing animated project" ng-repeat="project in projects | filter:query">
 					<!-- <span class="" ng-model="importance"></span> -->
 					<!-- <a href="" class="subtract" ng-click="minusOne($index)">-</a> -->
+					<a class="highlight-{{project.highlight}}" href="{{project.url}}">{{project.name}}</a>
 				</li>
 			</ul>
 			<br/>
 			<nav>
-				<a href="#" class="mode button" ng-model="mode" ng-click="toggleEditMode()">{{modeText}}</a>
+				<!-- <a href="#" class="mode button" ng-model="mode" ng-click="toggleEditMode()">{{modeText}}</a> -->
 				<?php echo '<a href="'. $project->path. '?refresh=true" class="button">Reset</a>'; ?>
 			</nav>
-
 		</div>
 	</body>
 </html>
